c295bc46bcfb43f3665b562fed5bd5633f8b458c
Mugenyi Allan <mugenyiallan88@gmail.com>
2025-05-26 13:02:57 +0300

    first assignment netlabs

---
+++ netLabs.py
@@ -0,0 +1,36 @@
+# # print("hello, my first day at netlabs")
+# # my_list = [1,2,3,4,5]
+# # x=my_list[-1]
+# # print(x)
+
+# mood_level =8
+# sleep_hours =7.5
+# is_favorite = True
+# print(type(mood_level))#<class'int'>
+# Mood_level= 7
+# print(f"mood:{Mood_level}") # how to write plus something else
+
+# print("Welcome")
+# # name=input("what is your name?")
+# mood =int(input("rate your mood (1-10)"))
+# date =input("whats the date?")
+# # print(f"Author:{name}")
+# print(mood)
+# print("\n"+"="*40); print(f"Date:{date}")
+# # print("="*40)
+# total_mood = mood+1+5
+# average_mood = total_mood/3
+# # if average_mood >19:
+# #     print(f"Average mood:{average_mood:.2f}")
+# # else:
+# #     print("please enter the correct mood ")
+
+# # for average_mood in range(11):
+# #     print
+
+today= "today has been AMAZING"
+x =today.strip().lower()
+y= today.split()
+z= today[0:5]
+print(y,z)
+print(f'split:{x}')
